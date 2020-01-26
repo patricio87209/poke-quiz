4ceef03afb1caf339efcd0b140b00d72e7c54e58
patricio <patricio87209@gmail.com>
2020-01-26 13:20:51 -0300

    Updates

---
+++ precache-manifest.867863a2431690230dd69914d1e9c928.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c2705c8c5d7f7597ac4f1219d2c198c",
+    "url": "/poke-quiz/index.html"
+  },
+  {
+    "revision": "fba0e36226bd56a7f42f",
+    "url": "/poke-quiz/static/css/main.002c7cfd.chunk.css"
+  },
+  {
+    "revision": "c5eb6506772b543efd2c",
+    "url": "/poke-quiz/static/js/2.c6adf3a6.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/poke-quiz/static/js/2.c6adf3a6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fba0e36226bd56a7f42f",
+    "url": "/poke-quiz/static/js/main.ccc02093.chunk.js"
+  },
+  {
+    "revision": "89e4ce158571102ae3ac",
+    "url": "/poke-quiz/static/js/runtime-main.fb99a025.js"
+  }
+]);
